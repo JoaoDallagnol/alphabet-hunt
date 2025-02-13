621ec8287a66a7b050be9fb46550cc02d7ed472b
Joao <progjvdallagnol2001@gmail.com>
2025-02-12 22:07:38 -0300

    Adding pixel art font and refactoring prototype to initical alphabet elements

--- Assets/Scripts/CollectGarbage.cs
+++ Assets/Scripts/CollectGarbage.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
 
-public class CollectGarbage : MonoBehaviour
-{
+public class CollectGarbage : MonoBehaviour {
+
+    private TextMeshPro collectableTextValue;
+
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Collectible") {
+            Debug.Log(collision.gameObject.GetComponent<TextMeshPro>().text);
             Destroy(collision.gameObject);
         }
     }
